9d61ff3eb69e4bd45ae24abe52861bf5d56aaf27
Jorge <jorge_fernandez_84@yahoo.es>
2024-10-17 20:05:28 +0200

    Summary

---
+++ variables.tf
@@ -0,0 +1,22 @@
+# below we define with the variable instance_count how many servers we want to create
+variable "instance_count" {
+  default = "3"
+}
+
+# below we define the default server names
+variable "instance_tags" {
+  type = list(string)
+  default = ["tf-ansible-1", "tf-ansible-2", "tf-ansible-3", "tf-ansible-4", "tf-ansible-5"]
+}
+
+# we use Ubuntu as the OS
+variable "ami" {
+  type = string
+  default = "ami-0084a47cc718c111a" #eu-central-1 image for x86_64 Ubuntu_20.04 2021-05-28T21:06:05.000Z
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
